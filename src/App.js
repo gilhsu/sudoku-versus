997a7baadaabf759b94820eadbbaf60e1cb64e4c
sketchbook21 <gilbert.hsu2@gmail.com>
2021-01-15 22:33:05 -0500

    config autodeploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
+        <p>Testing for Auto Deploy!</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
