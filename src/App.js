9622f9bef52103b5ee51df1a29b7d360de883da8
sketchbook21 <gilbert.hsu2@gmail.com>
2021-01-15 22:48:46 -0500

    change back to master

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
-        <p>Testing for Auto Deploy!</p>
+        <p>Testing for Master!</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
