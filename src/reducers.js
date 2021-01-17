19c47e07cb999c4570b10f23a938add482ca7f1d
sketchbook21 <gilbert.hsu2@gmail.com>
2021-01-16 21:01:00 -0500

    remove redux toolkit, remove react-redux

--- src/reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import settingsReducer from "./features/settings/settingsSlice";
-
-const rootReducer = combineReducers({
-  settings: settingsReducer,
-});
-
-export default rootReducer;
